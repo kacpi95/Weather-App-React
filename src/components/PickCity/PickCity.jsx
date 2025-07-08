938e90400a297eb91fa0bd6cc6e1f8439ad37b7f
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-08 11:56:00 +0200

    feat: function handleSubmit in PickCity

--- src/components/PickCity/PickCity.jsx
+++ src/components/PickCity/PickCity.jsx
@@ -3,12 +3,17 @@ import TextInput from '../TextInput/TextInput';
 import { useState } from 'react';
 import Button from '../Button/Button';
 
-export default function PickCity() {
+export default function PickCity({ action }) {
   const [city, setCIty] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    action({ city });
+  }
 
   return (
     <form className={styles.pickCityForm}>
-      <label>
+      <label onSubmit={handleSubmit}>
         <TextInput
           placeholder='Enter city name...'
           value={city}
